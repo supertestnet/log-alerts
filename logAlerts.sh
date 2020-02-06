414911c9848e45a2fde86b66401cd96b4c5966fb
supertestnet <58400631+supertestnet@users.noreply.github.com>
2020-02-05 21:08:36 -0500

    Update logAlerts.sh

--- logAlerts.sh
+++ logAlerts.sh
@@ -7,7 +7,7 @@ s2=$(cat lastline.txt);
 if [ "$s1" != "$s2" ];
 then
 echo "$s1" > ./lastline.txt
-curl https://incomingnotificationbot.alhur.es/n/yourKey -H 'content-type: text/plain' -d "$s1"
+curl https://incomingnotificationbot.alhur.es/n/yourKey -H 'content-type: text/plain' -d "$s1" #replace the phrase "yourKey" with the key you received from the notification bot
 echo "press ctrl+c to cancel"
 fi
 sleep 600
