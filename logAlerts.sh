939428a7a39e9963495ecadc92324e0de33b606d
supertestnet <58400631+supertestnet@users.noreply.github.com>
2020-02-05 21:07:32 -0500

    Update logAlerts.sh

--- logAlerts.sh
+++ logAlerts.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-s1=$(grep 'Z' test.txt | tail -1);
+echo "press ctrl+c to cancel"
+
+while true
+do
+s1=$(grep 'potentially earned' ./logs/yourLogFile.log | tail -1); #replace the phrase "yourLogFile" with the most recently updated log file in /scripts/logs/ 
 s2=$(cat lastline.txt);
 if [ "$s1" != "$s2" ];
 then
-echo $s1 > ./lastline.txt
-
-curl https://incomingnotificationbot.alhur.es/n/9aad08051cce12d6062c2dfac354d973 -H 'content-type: text/plain' -d "$s1"
-
+echo "$s1" > ./lastline.txt
+curl https://incomingnotificationbot.alhur.es/n/yourKey -H 'content-type: text/plain' -d "$s1"
+echo "press ctrl+c to cancel"
 fi
+sleep 600
+done
